a83afbe98571fef137f67f4ec8e3ebe72055a6dd
luis-k0-janis <luisk.net@gmail.com>
2019-02-23 23:12:52 -0300

    lesson 462

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,26 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const todo = props => {
   // useState returns an array, position 0 is the state, position 1 is the function to update the state
-  //   const [todoName, setTodoName] = useState(""); // array destructuring
-  //   const [todoList, setTodoList] = useState([]);
-  const [todoState, setTodoState] = useState({ userInput: "", todoList: [] });
+  const [todoName, setTodoName] = useState(""); // array destructuring
+  const [todoList, setTodoList] = useState([]);
+  // const [todoState, setTodoState] = useState({ userInput: "", todoList: [] });
+
+  // useEffect runs after each render cycle
+  useEffect(() => {
+    axios
+      .get("https://reacthooks-aec8d.firebaseio.com/todos.json")
+      .then(result => {
+        console.log(result);
+        const todoData = result.data;
+        const todos = [];
+        for (const key in todoData) {
+          todos.push({ id: key, name: todoData[key].name });
+        }
+        setTodoList(todos);
+      });
+  }, []);
 
   const inputChangeHandler = event => {
     // console.log(todoName);
-    // setTodoName(event.target.value); // with separated useState
-    setTodoState({
-      userInput: event.target.value,
-      todoList: todoState.todoList
-    });
+    setTodoName(event.target.value); // with separated useState
+    // setTodoState({
+    //   userInput: event.target.value,
+    //   todoList: todoState.todoList
+    // });
   };
 
   const todoAddHandler = () => {
-    // setTodoList(todoList.concat(todoName)); // concat returns a new array // with saparated useState
-    setTodoState({
-      userInput: todoState.userInput,
-      todoList: todoState.todoList.concat(todoState.userInput)
-    });
+    setTodoList(todoList.concat(todoName)); // concat returns a new array // with saparated useState
+    // setTodoState({
+    //   userInput: todoState.userInput,
+    //   todoList: todoState.todoList.concat(todoState.userInput)
+    // });
+    axios
+      .post("https://reacthooks-aec8d.firebaseio.com/todos.json", {
+        name: todoName
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   return (
@@ -29,15 +55,15 @@ const todo = props => {
         type="text"
         placeholder="Todo"
         onChange={inputChangeHandler}
-        value={todoState.userInput} // value={todoName}
+        value={todoName} //value={todoState.userInput} //
       />
       <button type="button" onClick={todoAddHandler}>
         Add
       </button>
       <ul>
-        {/* {todoList.map(todo => ( */}
-        {todoState.todoList.map(todo => (
-          <li key={todo}>{todo}</li>
+        {/* {todoState.todoList.map(todo => ( */}
+        {todoList.map(todo => (
+          <li key={todo.id}>{todo.name}</li>
         ))}
       </ul>
     </React.Fragment>
